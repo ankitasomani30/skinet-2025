89a9ef613c61c8260c8f5a71c2a62332edc128e0
Ankita Somani <ankita.somani95@gmail.com>
2025-12-23 19:43:22 +0530

    End of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  protected title = "Client";
+export class AppComponent implements OnInit {
+  baseUrl = 'http://localhost:5000/api';
+  protected title = "skinet";
+  private http = inject(HttpClient);
+  products : Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseUrl + '/products').subscribe({
+      next : response => this.products = response.data,
+      error: error => console.log(error),
+      complete : () => console.log("Request completed.")
+    })
+  }
 }
