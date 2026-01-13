c1333fb0bbf359a731ee089e21ca050ed621fc34
Ankita Somani <ankita.somani95@gmail.com>
2026-01-13 23:38:14 +0530

    End of section 30

---
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, input } from '@angular/core';
+import { CartItem } from '../../../shared/models/Cart';
+import { RouterLink } from "@angular/router";
+import { MatIconButton } from "@angular/material/button";
+import { MatIcon } from '@angular/material/icon';
+import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../../core/services/cart.service';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [RouterLink, MatIconButton, MatIcon, CurrencyPipe],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss',
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+  cartService = inject(CartService);
+
+
+  incementQuantity()
+  {
+    this.cartService.addItemToCart(this.item());
+  }
+
+  decrementQuantity()
+  {
+    this.cartService.removeItemFromCart(this.item().productId);
+  }
+
+  removeItemFromCart()
+  {
+    this.cartService.removeItemFromCart(this.item().productId, this.item().quantity); 
+  }
+}
