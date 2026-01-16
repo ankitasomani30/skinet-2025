8b194ec3ab8f20115783e44f2b7279768967e085
Ankita Somani <ankita.somani95@gmail.com>
2026-01-16 20:55:18 +0530

    End of section 14

--- API/Program.cs
+++ API/Program.cs
@@ -22,12 +22,16 @@ builder.Services.AddCors();
 builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 {
     var connString = builder.Configuration.GetConnectionString("Redis") 
-                    ?? throw new Exception("Cannot getreddis connection string");
+                    ?? throw new Exception("Cannot get redis connection string");
     var configuration = ConfigurationOptions.Parse(connString, true);
     return ConnectionMultiplexer.Connect(configuration);
 });
 
 builder.Services.AddSingleton<ICartService, CartService>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
@@ -38,6 +42,7 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().
 WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<AppUser>();
 
 try
 {
